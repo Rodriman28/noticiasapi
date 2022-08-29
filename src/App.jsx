5101dad80d4096c4c6f7ac304db0330e923e143a
Rodrigo <rodri.rirs@gmail.com>
2022-08-29 19:00:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container, Grid, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <header>
+        <Typography />
+      </header>
+    </Container>
+  );
+}
+
+export default App;

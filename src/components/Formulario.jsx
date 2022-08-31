b3259c33f785d320b93170e0b37caeaa426099bb
Rodrigo <rodri.rirs@gmail.com>
2022-08-31 12:00:03 -0300

    Provider, form, api conection

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,49 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Box,
+} from "@mui/material";
+import useNoticias from "../hooks/useNoticias";
+
+const CATEGORIAS = [
+  { value: "general", label: "General" },
+  { value: "business", label: "Negocios" },
+  { value: "entertainment", label: "Entretenimiento" },
+  { value: "health", label: "Salud" },
+  { value: "science", label: "Ciencia" },
+  { value: "sports", label: "Deportes" },
+  { value: "technology", label: "Tecnología" },
+];
+
+const Formulario = () => {
+  const { categoria, handleChangeCategoria } = useNoticias();
+
+  return (
+    <form>
+      <FormControl fullWidth>
+        <InputLabel>Categoria</InputLabel>
+        <Select
+          label="Categoría"
+          onChange={handleChangeCategoria}
+          value={categoria}
+        >
+          {CATEGORIAS.map((cat) => (
+            <MenuItem key={cat.value} value={cat.value}>
+              {cat.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <Box sx={{ marginTop: 2 }}>
+          <Button fullWidth variant="contained">
+            Buscar Noticias
+          </Button>
+        </Box>
+      </FormControl>
+    </form>
+  );
+};
+
+export default Formulario;
